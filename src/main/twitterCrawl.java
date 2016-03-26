a86ec76aec8c4e079ba8c57c75130a93ce47fb05
Sonam Gupta <sonagupta29@gmail.com>
2016-03-26 15:45:40 +0530

    Added files via upload

---
+++ src/main/twitterCrawl.java
@@ -0,0 +1,53 @@
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class twitterCrawl
+{
+	public static void main(String[] args) throws IOException, TwitterException {
+		
+		System.getProperties().put("http.proxyHost", "proxy.iiit.ac.in");
+		System.getProperties().put("http.proxyPort", "8080");
+		
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("ZvyHY7wciiK7WMHSvCLow8755")
+		  .setOAuthConsumerSecret("rirtzaACfiINS6nOaQajckqelWnDrqEMIQ3Hz4XTSaiFd8K5xD")
+		  .setOAuthAccessToken("3066359394-v3ExCcaKVxksplTxk3EmANIylScClNdZrjVsnq3")
+		  .setOAuthAccessTokenSecret("6dMcJElEZyQmq1JfvDm4Vw2DxI9Q4mdQhQPXUmFsOqW0P");
+		
+		TwitterFactory tf = new TwitterFactory(cb.build());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the search term.");
+        String searchTerm = scanner.nextLine();
+		Twitter twitter = tf.getInstance();
+		
+		try {
+		Query query = new Query(searchTerm);
+		QueryResult result;
+		int count=0;
+		
+		do {
+		result = twitter.search(query);
+		List<Status> tweets = result.getTweets();
+		System.out.println(searchTerm);
+
+		for (Status tweet : tweets) {
+			count++;
+			String s = tweet.getUser().getScreenName();
+			if (!s.toLowerCase().contains(searchTerm.toLowerCase()))
+				System.out.println("@" + s + " - " + tweet.getText());
+		}
+		} while ((query = result.nextQuery()) != null && count <= 10);
+		
+		System.exit(0);
+		} catch (TwitterException te) {
+		te.printStackTrace();
+		System.out.println("Failed to search tweets: " + te.getMessage());
+		System.exit(-1);
+		}
+	}
+}
